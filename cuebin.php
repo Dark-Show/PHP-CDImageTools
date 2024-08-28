f229e00354c4fcc963e2183e005d98ab5ae5c9ba
Greg Michalik <gregory954@gmail.com>
2024-08-28 16:08:33 -0600

    CLI app clean-ups

--- cuebin.php
+++ cuebin.php
@@ -12,104 +12,108 @@ include ('include/cuebin.php');
 const CUEBIN_MODE_CONCATENATE = 0;
 const CUEBIN_MODE_SPLIT = 1;
 
-if (count ($argv) == 1)
-	display_help ($argv);
+cli_process_argv ($argv);
 
-$out_dir = "./";
-$comp = CDROM_AUDIO_RAW;
-$mode = CUEBIN_MODE_CONCATENATE;
-for ($i = 1; $i < count ($argv); $i++) {
-	switch ($argv[$i]) {
-		case '-cue':
-			if (!isset ($argv[$i + 1]))
-				die ("Error: Invalid arguments\n");
-			$cue_file = $argv[$i + 1];
-			if (!is_file ($cue_file))
-				die ("Error: Can not access '$cue_file'\n");
-			$i++;
+function cli_process_argv ($argv) {
+	if (count ($argv) == 1)
+		cli_display_help ($argv);
+	
+	$out_dir = "./";
+	$comp = CDROM_AUDIO_RAW;
+	$mode = CUEBIN_MODE_CONCATENATE;
+	for ($i = 1; $i < count ($argv); $i++) {
+		switch ($argv[$i]) {
+			case '-cue':
+				if (!isset ($argv[$i + 1]))
+					die ("Error: Invalid arguments\n");
+				$cue_file = $argv[$i + 1];
+				if (!is_file ($cue_file))
+					die ("Error: Can not access '$cue_file'\n");
+				$i++;
+				break;
+			case '-basename':
+				if (!isset ($argv[$i + 1]))
+					die ("Error: Missing basename\n");
+				$basename = $argv[$i + 1];
+				$i++;
+				break;
+			case '-output':
+				if (!isset ($argv[$i + 1]))
+					die ("Error: Missing output directory\n");
+				$out_dir = $argv[$i + 1];
+				if (!is_dir ($out_dir) and !mkdir ($out_dir, 0777, true))
+					die ("Error: Could not create directory '$out_dir'\n");
+				if (substr ($out_dir, -1, 1) != '/')
+					$out_dir .= '/';
+				$i++;
+				break;
+			case '-concatenate':
+				$mode = CUEBIN_MODE_CONCATENATE;
+				break;
+			case '-split':
+				$mode = CUEBIN_MODE_SPLIT;
+				break;
+			case '-audio':
+				if (!isset ($argv[$i + 1]))
+					die ("Error: Missing audio format\n");
+				$mode = CUEBIN_MODE_SPLIT;
+				$comp = $argv[$i + 1];
+				switch ($comp) {
+					case 'raw':
+						$comp = CDROM_AUDIO_RAW;
+						break;
+					case 'wave':
+						$comp = CDROM_AUDIO_WAVE;
+						break;
+					case 'flac':
+						$comp = CDROM_AUDIO_FLAC;
+						break;
+					default:
+						die ("Error: Audio format not supported\n");
+				}
+				$i++;
+				break;
+			default:
+				cli_display_help ($argv);
+		}
+	}
+	if (!isset ($cue_file))
+		echo ("Error: No CUE file specified\n");
+
+	if (!isset ($basename)) {
+		$basename = explode ('/', $cue_file);
+		$basename = $basename[count ($basename) - 1];
+		$basename = substr ($basename, 0, -4);
+	}
+
+	$disk = cdrom_open_cue ($cue_file);
+
+	switch ($mode) {
+		case CUEBIN_MODE_CONCATENATE:
+			cdrom_concatenate_cue ($disk, $basename, $out_dir);
 			break;
-		case '-basename':
-			if (!isset ($argv[$i + 1]))
-				die ("Error: Missing basename\n");
-			$basename = $argv[$i + 1];
-			$i++;
-			break;
-		case '-output':
-			if (!isset ($argv[$i + 1]))
-				die ("Error: Missing output directory\n");
-			$out_dir = $argv[$i + 1];
-			if (!is_dir ($out_dir) and !mkdir ($out_dir, 0777, true))
-				die ("Error: Could not create directory '$out_dir'\n");
-			if (substr ($out_dir, -1, 1) != '/')
-				$out_dir .= '/';
-			$i++;
-			break;
-		case '-concatenate':
-			$mode = CUEBIN_MODE_CONCATENATE;
-			break;
-		case '-split':
-			$mode = CUEBIN_MODE_SPLIT;
-			break;
-		case '-audio':
-			if (!isset ($argv[$i + 1]))
-				die ("Error: Missing audio format\n");
-			$mode = CUEBIN_MODE_SPLIT;
-			$comp = $argv[$i + 1];
-			switch ($comp) {
-				case 'raw':
-					$comp = CDROM_AUDIO_RAW;
-					break;
-				case 'wave':
-					$comp = CDROM_AUDIO_WAVE;
-					break;
-				case 'flac':
-					$comp = CDROM_AUDIO_FLAC;
-					break;
-				default:
-					die("Error: Audio format not supported\n");
-			}
-			$i++;
+		case CUEBIN_MODE_SPLIT:
+			cdrom_split_cue ($disk, $basename, $out_dir, $comp);
 			break;
 		default:
-			display_help ($argv);
+			die();
 	}
-}
-if (!isset ($cue_file))
-	echo ("Error: No CUE file specified\n");
-
-if (!isset ($basename)) {
-	$basename = explode ('/', $cue_file);
-	$basename = $basename[count ($basename) - 1];
-	$basename = substr ($basename, 0, -4);
+	cdrom_save_cue ($disk, "$out_dir$basename.cue");
 }
 
-$disk = cdrom_open_cue ($cue_file);
-
-switch ($mode) {
-	case CUEBIN_MODE_CONCATENATE:
-		cdrom_concatenate_cue ($disk, $basename, $out_dir);
-		break;
-	case CUEBIN_MODE_SPLIT:
-		cdrom_split_cue ($disk, $basename, $out_dir, $comp);
-		break;
-	default:
-		die();
-}
-cdrom_save_cue ($disk, "$out_dir$basename.cue");
-
-function display_help ($argv) {
-	echo ("Cuebin Tools v" . VERSION . "\n" .
-		  "  Arguments:\n" .
-		  "    -cue \"FILE.CUE\"          Input CUE file\n" .
-		  "    -basename \"DISC_NAME\"    Output filename\n" .
-		  "    -output \"PATH/\"          Output directory\n" .
-		  "    -concatenate             Concatenate CUE/BIN files\n" .
-		  "    -split                   Split CUE/BIN into file per track\n" .
-		  "    -audio flac|wave|raw     Compress audio tracks to format\n\n" .
-		  "  Example Usages:\n" .
-		  "    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -split\n" .
-		  "    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -concatenate\n" .
-		  "    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -audio flac\n");
+function cli_display_help ($argv) {
+	echo ("Cuebin Conversion Tool v" . VERSION . "\n");
+	echo ("  Arguments:\n");
+	echo ("    -cue \"FILE.CUE\"          Input CUE file\n");
+	echo ("    -basename \"DISC_NAME\"    Output filename\n");
+	echo ("    -output \"PATH/\"          Output directory\n");
+	echo ("    -concatenate             Concatenate CUE/BIN files\n");
+	echo ("    -split                   Split CUE/BIN into file per track\n");
+	echo ("    -audio flac|wave|raw     Compress audio tracks to format\n\n");
+	echo ("  Example Usages:\n");
+	echo ("    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -split\n");
+	echo ("    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -concatenate\n");
+	echo ("    " . $argv[0] . " -cue \"input.cue\" -output \"output/\" -audio flac\n");
 	die();
 }
 
--- imgdump.php
+++ imgdump.php
@@ -10,73 +10,12 @@ include('./include/cdrom/cdemu.php');
 include('./include/cdrom/iso9660.php');
 //include('./include/binary.php'); // DEBUG FUNCTIONS
 
-if (count ($argv) == 1)
-	cli_display_help ($argv);
-else
-	cli_process_argv ($argv);
-
-// Dump image loaded by cdemu
-function dump_image ($cdemu, $dir_out, $remove_version = false) {
-	for ($track = 1; $track <= $cdemu->get_track_count(); $track++) {
-		$t = str_pad ($track, 2, '0', STR_PAD_LEFT);
-		echo ("  Track $t\n");
-		if (!$cdemu->set_track ($track))
-			die ("Error: Unexpected end of image!\n");
-		if ($cdemu->get_track_type() == 1) { // Data
-			if (!is_dir ($dir_out . "Track $t"))
-				mkdir ($dir_out . "Track $t", 0777, true);
-			dump_data ($cdemu, $dir_out . "Track $t/", $remove_version);
-		} else // Audio
-			dump_audio ($cdemu, $dir_out . "Track $t.cdda");
-	}
-	//print_r ($cdemu->get_sector_access_list());
-	$cdemu->eject(); // Eject Disk
-}
-function dump_audio ($cdemu, $file_out) {
-	$fp = fopen ($file_out, 'w');
-	$s_len = $cdemu->get_track_length (true);
-	for ($s_cur = 0; $s_cur < $s_len; $s_cur++){
-    	$sector = $cdemu->read();
-		if (isset ($sector['data']))
-			fputs ($fp, $sector['data']);
-		cli_dump_progress ($s_len * 2352, $cdemu->get_track_time (true) * 2352);
-	}
-	fclose ($fp);
-	return (true);
-}
-
-// Dump File System Contents
-function dump_data ($cdemu, $dir_out, $remove_version = false) {
-	$iso9660 = new ISO9660;
-	$iso9660->set_data_read ([$cdemu, 'read']);
-	$iso9660->init(); // Load filesystem
-	//print_r ($iso9660->get_pvd());
-	//die();
-	if (!is_dir ($dir_out . "contents"))
-		mkdir ($dir_out . "contents", 0777, true);
-	$iso9660->save_system_area ($dir_out . "system_area.bin");
-	$contents = $iso9660->list_contents(); // List contents recursively
-	foreach ($contents as $c) { // Save contents to disk
-		echo ("    $c\n");
-		if (substr ($c, -1, 1) == '/') { // Directory check
-			if (!is_dir ($dir_out . "contents/$c"))
-				mkdir ($dir_out . "contents/$c", 0777, true);
-			continue;
-		}
-		$f = $remove_version ? $iso9660->format_filename ($c) : $c; // Remove version from filename
-		$iso9660->save_file ($c, $dir_out . "contents/$f", false, 'cli_dump_progress');
-	}
-	return (true);
-}
-
-function cli_dump_progress ($length, $pos) {
-	$p = $length > 0 ? floor ($pos / $length * 100) : 0;
-	echo ($cli = "      $pos / $length = $p%\r");
-	if ($p == 100)
-		echo (str_repeat (' ', strlen ($cli)) . "\r");
-}
+cli_process_argv ($argv);
 
 function cli_process_argv ($argv) {
+	if (count ($argv) == 1)
+		cli_display_help ($argv);
+	
 	$remove_version = false;
 	$dir_out = "output/";
 	for ($i = 1; $i < count ($argv); $i++) {
@@ -139,6 +78,68 @@ function cli_process_argv ($argv) {
 		}
 	}
 	dump_image ($cdemu, $dir_out, $remove_version);
+	
+	//print_r ($cdemu->get_sector_access_list());
+	$cdemu->eject(); // Eject Disk
+}
+
+// Dump image loaded by cdemu
+function dump_image ($cdemu, $dir_out, $remove_version = false) {
+	for ($track = 1; $track <= $cdemu->get_track_count(); $track++) {
+		$t = str_pad ($track, 2, '0', STR_PAD_LEFT);
+		echo ("  Track $t\n");
+		if (!$cdemu->set_track ($track))
+			die ("Error: Unexpected end of image!\n");
+		if ($cdemu->get_track_type() == 1) { // Data
+			if (!is_dir ($dir_out . "Track $t"))
+				mkdir ($dir_out . "Track $t", 0777, true);
+			dump_data ($cdemu, $dir_out . "Track $t/", $remove_version);
+		} else // Audio
+			dump_audio ($cdemu, $dir_out . "Track $t.cdda");
+	}
+}
+function dump_audio ($cdemu, $file_out) {
+	$fp = fopen ($file_out, 'w');
+	$s_len = $cdemu->get_track_length (true);
+	for ($s_cur = 0; $s_cur < $s_len; $s_cur++){
+    	$sector = $cdemu->read();
+		if (isset ($sector['data']))
+			fputs ($fp, $sector['data']);
+		cli_dump_progress ($s_len * 2352, $cdemu->get_track_time (true) * 2352);
+	}
+	fclose ($fp);
+	return (true);
+}
+
+// Dump File System Contents
+function dump_data ($cdemu, $dir_out, $remove_version = false) {
+	$iso9660 = new ISO9660;
+	$iso9660->set_data_read ([$cdemu, 'read']);
+	$iso9660->init(); // Load filesystem
+	//print_r ($iso9660->get_pvd());
+	//die();
+	if (!is_dir ($dir_out . "contents"))
+		mkdir ($dir_out . "contents", 0777, true);
+	$iso9660->save_system_area ($dir_out . "system_area.bin");
+	$contents = $iso9660->list_contents(); // List contents recursively
+	foreach ($contents as $c) { // Save contents to disk
+		echo ("    $c\n");
+		if (substr ($c, -1, 1) == '/') { // Directory check
+			if (!is_dir ($dir_out . "contents/$c"))
+				mkdir ($dir_out . "contents/$c", 0777, true);
+			continue;
+		}
+		$f = $remove_version ? $iso9660->format_filename ($c) : $c; // Remove version from filename
+		$iso9660->save_file ($c, $dir_out . "contents/$f", false, 'cli_dump_progress');
+	}
+	return (true);
+}
+
+function cli_dump_progress ($length, $pos) {
+	$p = $length > 0 ? floor ($pos / $length * 100) : 0;
+	echo ($cli = "      $pos / $length = $p%\r");
+	if ($p == 100)
+		echo (str_repeat (' ', strlen ($cli)) . "\r");
 }
 
 function cli_display_help ($argv) {
