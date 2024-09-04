1032605cf36017c77052f439277358c6db562027
Greg Michalik <gregory954@gmail.com>
2024-09-03 21:52:40 -0600

    iso9660: Small bug-fix

--- include/cdrom/iso9660.php
+++ include/cdrom/iso9660.php
@@ -270,10 +270,12 @@ class ISO9660 {
 	private function process_volume_descriptor() {
 		$loc = 16;
 		do {
-			if (($data = $this->o_cdemu->read ($loc++)) === false) // Load Sector 16
+			if (($data = $this->o_cdemu->read ($loc++)) === false)
 				return (false);
 			if (($vd = $this->volume_descriptor ($data['data'])) === false or $vd['type'] == 255)
 				$loc = false;
+			if ($vd === false)
+				continue;
 			if ($this->iso_vd === false)
 				$this->iso_vd = array ();
 			$this->iso_vd[$vd['type']] = $vd;
